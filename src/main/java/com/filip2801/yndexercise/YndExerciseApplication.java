05ff8dd307a04d22684993b7106bd18031ff19a2
Filip Karaś <filip2801@gmail.com>
2021-02-07 18:41:37 +0100

    Initialize project

---
+++ src/main/java/com/filip2801/yndexercise/YndExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.filip2801.yndexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YndExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YndExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filip2801/yndexercise/YndExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip2801.yndexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YndExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
