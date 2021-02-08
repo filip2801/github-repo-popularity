103ddd977cc5b01201899d3ca9640b19f514a62e
Filip Karaś <filip2801@gmail.com>
2021-02-09 00:41:02 +0100

    Add Swagger docs

--- src/main/java/com/filip2801/githubrepopopularity/web/RepositoryPopularityController.java
+++ src/main/java/com/filip2801/githubrepopopularity/web/RepositoryPopularityController.java
@@ -2,6 +2,10 @@ package com.filip2801.githubrepopopularity.web;
 
 import com.filip2801.githubrepopopularity.domain.RepositoryId;
 import com.filip2801.githubrepopopularity.domain.RepositoryPopularityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,11 @@ public class RepositoryPopularityController {
 		this.repositoryPopularityService = repositoryPopularityService;
 	}
 
+	@Operation(summary = "Check if github repository is popular")
+	@ApiResponses({
+		@ApiResponse(responseCode = "404", description = "Repository does not exist", content = @Content),
+		@ApiResponse(responseCode = "502", description = "Github api does not respond", content = @Content)
+	})
 	@GetMapping("/repositories/{owner}/{repositoryName}/popularity")
 	public RepositoryPopularityResource getRepositoryPopularity(@PathVariable String owner, @PathVariable String repositoryName) {
 
