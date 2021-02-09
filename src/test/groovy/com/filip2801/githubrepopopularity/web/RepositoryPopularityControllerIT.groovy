171ba97bfe248a390b6d7141a3dfea1a26db46f4
Filip Karaś <filip2801@gmail.com>
2021-02-09 22:51:01 +0100

    Clean up tests

--- src/test/groovy/com/filip2801/githubrepopopularity/web/RepositoryPopularityControllerIT.groovy
+++ src/test/groovy/com/filip2801/githubrepopopularity/web/RepositoryPopularityControllerIT.groovy
@@ -1,7 +1,11 @@
 package com.filip2801.githubrepopopularity.web
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+
 import com.filip2801.githubrepopopularity.IntegrationTestSpecification
-import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.junit.WireMockRule
 import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.RESTClient
@@ -9,7 +13,6 @@ import org.junit.Rule
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import spock.lang.Shared
 
 class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
@@ -29,7 +32,7 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 
 	def setup() {
 		restClient.setUri("http://localhost:${port}${contextPath}/")
-		restClient.setContentType(MediaType.APPLICATION_JSON_VALUE)
+		restClient.setContentType(APPLICATION_JSON_VALUE)
 		wireMockRule.resetAll()
 	}
 
@@ -46,10 +49,10 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 		def repositoryOwner = "some-owner"
 		def repositoryName = "some-repo-name"
 
-		wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
-								 .willReturn(WireMock.aResponse()
+		wireMockRule.stubFor(get(urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
+								 .willReturn(aResponse()
 												 .withStatus(200)
-												 .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+												 .withHeader("Content-Type", APPLICATION_JSON_VALUE)
 												 .withBody("""{
 													"stargazers_count": 100,
 													"forks_count": 300
@@ -57,7 +60,7 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 
 
 		when:
-		def response = restClient.get(path: "/repositories/${repositoryOwner}/${repositoryName}/popularity") as HttpResponseDecorator
+		def response = callEndpoint(repositoryOwner, repositoryName)
 
 		then:
 		response.status == HttpStatus.OK.value()
@@ -70,12 +73,11 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 		def repositoryOwner = "some-owner"
 		def repositoryName = "some-not-existing-repo"
 
-		wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
-								 .willReturn(WireMock.aResponse()
-												 .withStatus(404)))
+		wireMockRule.stubFor(get(urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
+								 .willReturn(aResponse().withStatus(404)))
 
 		when:
-		def response = restClient.get(path: "/repositories/${repositoryOwner}/${repositoryName}/popularity") as HttpResponseDecorator
+		def response = callEndpoint(repositoryOwner, repositoryName)
 
 		then:
 		response.status == HttpStatus.NOT_FOUND.value()
@@ -86,12 +88,12 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 		def repositoryOwner = "some-owner"
 		def repositoryName = UUID.randomUUID().toString()
 
-		wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
-								 .willReturn(WireMock.aResponse()
+		wireMockRule.stubFor(get(urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
+								 .willReturn(aResponse()
 												 .withStatus(500)))
 
 		when:
-		def response = restClient.get(path: "/repositories/${repositoryOwner}/${repositoryName}/popularity") as HttpResponseDecorator
+		def response = callEndpoint(repositoryOwner, repositoryName)
 
 		then:
 		response.status == HttpStatus.BAD_GATEWAY.value()
@@ -102,21 +104,25 @@ class RepositoryPopularityControllerIT extends IntegrationTestSpecification {
 		def repositoryOwner = "some-owner"
 		def repositoryName = UUID.randomUUID().toString()
 
-		wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
-								 .willReturn(WireMock.aResponse()
+		wireMockRule.stubFor(get(urlEqualTo("/repos/${repositoryOwner}/${repositoryName}"))
+								 .willReturn(aResponse()
 												 .withStatus(200)
 												 .withFixedDelay(20)
-												 .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+												 .withHeader("Content-Type", APPLICATION_JSON_VALUE)
 												 .withBody("""{
 													"stargazers_count": 100,
 													"forks_count": 300
 													}""")))
 
 		when:
-		def response = restClient.get(path: "/repositories/${repositoryOwner}/${repositoryName}/popularity") as HttpResponseDecorator
+		def response = callEndpoint(repositoryOwner, repositoryName)
 
 		then:
 		response.status == HttpStatus.BAD_GATEWAY.value()
+	}
+
+	private HttpResponseDecorator callEndpoint(repositoryOwner, repositoryName) {
+		return restClient.get(path: "/repositories/${repositoryOwner}/${repositoryName}/popularity") as HttpResponseDecorator
 	}
 
 }
